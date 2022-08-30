b22ae3da7f863aa40a95e21e35b6bcdf34db096a
andre <andres.hoyos18@eia.edu.co>
2022-08-30 16:36:34 -0500

    .

--- main.c
+++ main.c
@@ -5,37 +5,7 @@ void duplicarValor(int);
 void duplicarReferencia(int *);
 
 int main() {
-    printf("Hello, World!\n");
-    fflush(stdout);
-    int i = 0;
-    scanf_s("%d", &i);
+    printf("Hello World");
 
-    printf("Ingrese un numero para duplicar\n");
-    fflush(stdout);
-    i = 0;
-    scanf_s("%d", &i);
-
-    printf("Ingresaste %10d esta en %p", i, &i);
     return 0;
 }
-
-void duplicarValor(int n) {
-    printf("---Duplicar por Valor---\n");
-    printf("%-10s %d\n", "n", n);
-    printf("%-10s %p\n", "&n", &n);
-    n = n * 2;
-    printf("%-10s %d\n", "n", n);
-    printf("%-10s %p\n", "&n", &n);
-    printf("---\n");
-}
-
-void duplicarReferencia(int *n) {
-    printf("---Duplicar por Referencia---\n");
-    printf("%-10s %d\n", "n", *n);
-    printf("%-10s %p\n", "&n", n);
-    *n = (*n) * 2;
-    printf("%-10s %d\n", "n", *n);
-    printf("%-10s %p\n", "&n", n);
-    printf("---\n");
-}
-
